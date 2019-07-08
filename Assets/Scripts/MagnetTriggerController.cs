c0822268a1296492aa9d8a562278521b13838d01
ochoajulian <julianochoa95@gmail.com>
2019-07-08 17:17:38 -0400

    Initial commit

---
+++ Assets/Scripts/MagnetTriggerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnetTriggerController : MonoBehaviour
+{
+
+    public Magnet leftHand, rightHand;
+    private float rightTrigger, leftTrigger;
+    public float maxForce = 10f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rightTrigger = Input.GetAxis("Oculus_CrossPlatform_SecondaryIndexTrigger");
+        leftTrigger = Input.GetAxis("Oculus_CrossPlatform_PrimaryIndexTrigger");
+
+        leftHand.MagnetForce = maxForce * leftTrigger;
+        rightHand.MagnetForce = maxForce * rightTrigger;
+    }
+}
